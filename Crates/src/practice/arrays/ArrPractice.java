1ea7593d55afaf9b011d4edd54b51b192110029b
clicker57 <hockidoggy@gmail.com>
2017-09-12 19:59:59 +0800

    compare 4 ways of list traverse

--- Crates/src/practice/arrays/ArrPractice.java
+++ Crates/src/practice/arrays/ArrPractice.java
@@ -1,22 +1,82 @@
 package practice.arrays;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by Philip on 7/5/16.
  */
 public class ArrPractice {
-    public boolean has23(int[] nums) {
-        Math.max(nums[0], nums[nums.length-1]);
 
-        for (int i: nums) {
-            System.out.print(" "+nums[i]);
+    private List<String> list = new ArrayList<>();
+
+    public ArrPractice() {
+        int num = 10000000;
+
+        for (int i = 0; i < num; i++) {
+            list.add("list" + i);
         }
-        System.out.println();
-        return true;
     }
 
     public static void main(String[] args) {
         ArrPractice obj = new ArrPractice();
 
-        obj.has23(new int[] {0});
+        obj.foreach();
+        obj.forlist();
+        obj.iteratorList();
+        obj.forArray();
+    }
+
+
+
+    void foreach() {
+        long start = System.currentTimeMillis();
+
+        for (String s : list) {
+            String value = s;
+        }
+
+        long end = System.currentTimeMillis();
+        long count = end - start;
+        System.out.println("foreach list : " + count);
+    }
+
+    void forlist() {
+        long start = System.currentTimeMillis();
+
+        for (int i=0; i < list.size(); i++) {
+            String value = list.get(i);
+        }
+
+        long end = System.currentTimeMillis();
+        long count = end - start;
+        System.out.println("for list.size() : " + count);
+    }
+
+    void iteratorList() {
+        long start = System.currentTimeMillis();
+
+        for (Iterator<String> it = list.iterator(); it.hasNext(); ) {
+            String value = it.next();
+        }
+
+        long end = System.currentTimeMillis();
+        long count = end - start;
+        System.out.println("iterator : " + count);
+    }
+
+    void forArray() {
+        long start = System.currentTimeMillis();
+
+        String[] strArr = new String[list.size()];
+        list.toArray(strArr);
+        for (String s : strArr) {
+            String value = s;
+        }
+
+        long end = System.currentTimeMillis();
+        long count = end - start;
+        System.out.println("forArray : " + count);
     }
 }
