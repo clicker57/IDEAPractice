50660d14e0e06ea219686645b278cbbf59fd4fef
clicker57 <hockidoggy@gmail.com>
2017-11-25 20:55:30 +0800

    update code

--- Crates/src/practice/BasicPractice.java
+++ Crates/src/practice/BasicPractice.java
@@ -3,25 +3,10 @@ package practice;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class BasicPractice {
-    public boolean equalIsNot(String str) {
-        if (str==null || "".equals(str) || str.length()==1) { return true; }
-
-        int isNum=0, notNum=0;
-        for (int i=0; i<str.length()-1; i++) {
-            if (i+2<=str.length() && str.substring(i,i+2).equals("is")) {
-                isNum++;
-            }
-            if (i+3<=str.length() && str.substring(i,i+3).equals("not")) {
-                notNum++;
-            }
-        }
-
-        return isNum == notNum;
-    }
 
     public static void main(String[] args) {
         long beginT;
@@ -36,6 +21,10 @@ public class BasicPractice {
         double dd = 1234.5993;
         DecimalFormat df = new DecimalFormat("#.##");
         System.out.println("decimalformat: " + df.format(dd));
+
+        Calendar now = Calendar.getInstance();
+        System.out.println("month: " + now.get(Calendar.MONTH));
+
     }
 
     private void parseDoc(String fileName) {
