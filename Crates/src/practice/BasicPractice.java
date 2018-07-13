1e693342ac7b0e533075fb651d52d5a1d2fa5f88
clicker57 <hockidoggy@gmail.com>
2018-07-13 10:22:11 +0800

    根据 AJCG 格式化代码

--- Crates/src/practice/BasicPractice.java
+++ Crates/src/practice/BasicPractice.java
@@ -2,10 +2,15 @@ package practice;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.security.MessageDigest;
 import java.text.DecimalFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * @author Philip
+ */
 public class BasicPractice {
 
     public static void main(String[] args) {
@@ -15,28 +20,59 @@ public class BasicPractice {
         System.currentTimeMillis();
 
         BasicPractice obj = new BasicPractice();
-        obj.parseDoc("/Users/Philip/workspaces/jdkprac/l.man");
 
-
-        double dd = 1234.5993;
-        DecimalFormat df = new DecimalFormat("#.##");
-        System.out.println("decimalformat: " + df.format(dd));
-
-        Calendar now = Calendar.getInstance();
-        System.out.println("month: " + now.get(Calendar.MONTH));
+//        double dd = 1234.5993;
+//        DecimalFormat df = new DecimalFormat("#.##");
+//        System.out.println("decimal format: " + df.format(dd));
 
     }
 
-    private void parseDoc(String fileName) {
-        try (Scanner in = new Scanner(new FileInputStream(fileName))) {
-            while (in.hasNext()) {
-                System.out.println(in.next());
+    public static String hexStr2Str(String hexStr) {
+        String str = "0123456789ABCDEF";
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            n = str.indexOf(hexs[2 * i]) * 16;
+            n += str.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes);
+    }
+
+
+    public static final String MD5(String pwd) {
+        //用于加密的字符
+        char[] md5String = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F' };
+        try {
+            //使用平台的默认字符集将此 String 编码为 byte序列，并将结果存储到一个新的 byte数组中
+            byte[] btInput = pwd.getBytes();
+
+            //信息摘要是安全的单向哈希函数，它接收任意大小的数据，并输出固定长度的哈希值。
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+
+            //MessageDigest对象通过使用 update方法处理数据， 使用指定的byte数组更新摘要
+            mdInst.update(btInput);
+
+            // 摘要更新之后，通过调用digest（）执行哈希计算，获得密文
+            byte[] md = mdInst.digest();
+
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char[] str = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = md5String[byte0 >>> 4 & 0xf];
+                str[k++] = md5String[byte0 & 0xf];
             }
-            System.out.println("***** try ends *****");
-        } catch (FileNotFoundException e) {
-            System.out.println("---> " + e.getMessage());
-            e.printStackTrace();
-            System.out.println("---> catch ends");
+
+            //返回经过加密后的字符串
+            return new String(str);
+
+        } catch (Exception e) {
+            return null;
         }
     }
 }
